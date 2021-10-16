f8b457e05825b1aef3bf70f4591a823df817d3c2
“CaiZhanrui” <zhanruicai@foxmail.com>
2021-10-15 23:40:46 -0400

    initial commit

---
+++ R/data_functions.R
@@ -0,0 +1,20 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+#' Create BMI Groups
+#'
+#' Converts continuous BMI variable to BMI groups.
+#'
+#' @param x Numeric vector.
+#'
+#' @return Factor variable.
+#'
+#' @examples
+#' bmi.vals <- rnorm(n = 50, mean = 25, sd = 3)
+#'
+#' @export
+bmi3 <- function(x) {
+  return(x)
+}
